18a7d087c52e9f8a08c33db2f6035fb004170ad1
Mohamed-Salem-Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-09-08 23:59:40 +0300

    Update data_handler.py

---
+++ Project/data_handler.py
@@ -0,0 +1,32 @@
+# data_handler.py
+import json
+from book import Book
+from member import Member
+from library import Library
+
+def save_data(library):
+    books_data = {book_id: book.__dict__ for book_id, book in library.books.items()}
+    members_data = {member_id: member.__dict__ for member_id, member in library.members.items()}
+
+    with open('library_data.json', 'w') as file:
+        json.dump({"books": books_data, "members": members_data}, file)
+
+def load_data():
+    try:
+        with open('library_data.json', 'r') as file:
+            data = json.load(file)
+
+            library = Library()
+            for book_id, book_info in data['books'].items():
+                book = Book(**book_info)
+                library.books[book_id] = book
+
+            for member_id, member_info in data['members'].items():
+                member = Member(**member_info)
+                library.members[member_id] = member
+
+            print("Library data loaded.")
+            return library
+    except FileNotFoundError:
+        print("No previous data found.")
+        return Library()
