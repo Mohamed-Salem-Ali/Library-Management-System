1f793014dfb8d5a822b8d7dd51f2480fb202b253
Mohamed-Salem-Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-09-09 01:57:27 +0300

    Update member.py

--- Project/member.py
+++ Project/member.py
@@ -1,12 +1,12 @@
 # member.py
 
 class Member:
-    def __init__(self, member_id, name, role='user'):
+    def __init__(self, member_id, name, role='user', borrowed_books=None):
         self.member_id = member_id
         self.name = name
-        self.role = role  
-        self.borrowed_books = []
-
+        self.role = role
+        self.borrowed_books = borrowed_books if borrowed_books else []  # Default to an empty list if None
+        
     def borrow_book(self, book):
         if book.available:
             self.borrowed_books.append(book.book_id)
